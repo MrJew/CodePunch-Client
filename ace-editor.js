cca9bbf6ab50f2d4493594635f750080a9a483e4
MrJew <kouzmov@gmail.com>
2015-06-20 19:08:16 +0100

    example objectives added

---
+++ ace-editor.js
@@ -0,0 +1,5 @@
+ar editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.getSession().setMode("ace/mode/python");
+var editor = ace.edit(document.getElementById('test'));
+editor.getSession().setUseWorker(false);
